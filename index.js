ff2024b53a3ea667aea7f8d0384c2a8fef7eff62
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-03-11 20:14:16 -0800

    Fix require statements in index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var automation = require('lib/automation'),
-    deploy     = require('lib/deploy');
-
+var automation = require('./lib/automation'),
+    deploy     = require('./lib/deploy');
 
 exports.automation = automation;
 exports.deploy = deploy;
