123c94bd5d19e97ac8b993e637cda8d55c55fbc5
Irene Morente <imorente@gmail.com>
2018-05-31 14:46:13 -0700

    Don't show jekyll warning for hexo sites

--- src/automation.js
+++ src/automation.js
@@ -1,4 +1,4 @@
-var TOML = require('toml');
+var TOML = require("toml");
 
 /*
   Heruistics for continuous deployment setups.
@@ -15,13 +15,13 @@ var TOML = require('toml');
     'read'  - read the content of this file
 */
 export const files = {
-  '_config.yml': 'check',
-  'netlify.toml': 'read',
-  'Gemfile': 'read',
-  'package.json': 'read',
-  'app.coffee': 'check',
-  'requirements.txt': 'read',
-  'yarn.lock': 'check'
+  "_config.yml": "check",
+  "netlify.toml": "read",
+  Gemfile: "read",
+  "package.json": "read",
+  "app.coffee": "check",
+  "requirements.txt": "read",
+  "yarn.lock": "check"
 };
 
 /*
@@ -63,9 +63,11 @@ export function settings(files = {}) {
 }
 
 function netlifyTomlSettings(files) {
-  if (!files['netlify.toml']) { return; }
+  if (!files["netlify.toml"]) {
+    return;
+  }
 
-  const config = TOML.parse(files['netlify.toml']);
+  const config = TOML.parse(files["netlify.toml"]);
   if (config) {
     const settings = {};
     if (config.build) {
@@ -81,57 +83,61 @@ function netlifyTomlSettings(files) {
 }
 
 const gems = {
-  jekyll: {cmd: 'jekyll build', dir: '_site/'},
-  middleman: {cmd: 'middleman build', dir: 'build/'},
-  nanoc: {cmd: 'nanoc', dir: 'output/'}
+  jekyll: { cmd: "jekyll build", dir: "_site/" },
+  middleman: { cmd: "middleman build", dir: "build/" },
+  nanoc: { cmd: "nanoc", dir: "output/" }
 };
 
 function gemfileSettings(files) {
-  if (!files['Gemfile']) { return; }
+  if (!files["Gemfile"]) {
+    return;
+  }
 
   for (var i in gems) {
     var regexp = new RegExp(`('${i}'|"${i}")`);
-    if (regexp.test(files['Gemfile'])) {
+    if (regexp.test(files["Gemfile"])) {
       return gems[i];
     }
   }
 }
 
 const packages = {
-  brunch: {cmd: "brunch build", dir: "public/"},
-  assemble: {cmd: "grunt build", dir: "dist/"},
-  'ember-cli': {cmd: "ember build -e production", dir: "dist/"},
-  hexo: {cmd: "hexo generate", dir: "public/"},
-  metalsmith: {cmd: "metalsmith", dir: "build/"},
-  roots: {cmd: "roots compile", dir: "public/"},
-  docpad: {cmd: "docpad generate", dir: "out/"},
-  wintersmith: {cmd: "wintersmith build", dir: "build/"},
-  gatsby: {cmd: "gatsby build", dir: "public/"},
-  harp: {cmd: "harp compile", dir: "www/"},
-  grunt: {cmd: "grunt build", dir: "dist/"},
-  gulp: {cmd: "gulp build", dir: "dist/"}
-}
+  brunch: { cmd: "brunch build", dir: "public/" },
+  assemble: { cmd: "grunt build", dir: "dist/" },
+  "ember-cli": { cmd: "ember build -e production", dir: "dist/" },
+  hexo: { cmd: "hexo generate", dir: "public/" },
+  metalsmith: { cmd: "metalsmith", dir: "build/" },
+  roots: { cmd: "roots compile", dir: "public/" },
+  docpad: { cmd: "docpad generate", dir: "out/" },
+  wintersmith: { cmd: "wintersmith build", dir: "build/" },
+  gatsby: { cmd: "gatsby build", dir: "public/" },
+  harp: { cmd: "harp compile", dir: "www/" },
+  grunt: { cmd: "grunt build", dir: "dist/" },
+  gulp: { cmd: "gulp build", dir: "dist/" }
+};
 
 function packageSettings(files) {
-  if (!files['package.json']) { return; }
+  if (!files["package.json"]) {
+    return;
+  }
   var data, dependencies, devDependencies;
   try {
-    data = JSON.parse(files['package.json']);
-  } catch(e) {
+    data = JSON.parse(files["package.json"]);
+  } catch (e) {
     return;
   }
 
-  var runCommand = files['yarn.lock'] ? 'yarn' : 'npm run';
+  var runCommand = files["yarn.lock"] ? "yarn" : "npm run";
 
   dependencies = data.dependencies || {};
   devDependencies = data.devDependencies || {};
 
   if (devDependencies["react-scripts"] || dependencies["react-scripts"]) {
-    return {cmd: `${runCommand} build`, dir: "build/"};
+    return { cmd: `${runCommand} build`, dir: "build/" };
   }
 
   if (packages.scripts && packages.scripts.build) {
-    return {cmd: `${runCommand} build`, dir: "dist/"};
+    return { cmd: `${runCommand} build`, dir: "dist/" };
   }
 
   for (var i in packages) {
@@ -142,69 +148,89 @@ function packageSettings(files) {
 }
 
 const requirements = {
-  mkdocs: {cmd: "mkdocs build", dir: "site/"},
-  pelican: {cmd: "pelican content", dir: "output/"},
-  cactus: {cmd: "cactus build", dir: ".build/"}
+  mkdocs: { cmd: "mkdocs build", dir: "site/" },
+  pelican: { cmd: "pelican content", dir: "output/" },
+  cactus: { cmd: "cactus build", dir: ".build/" }
 };
 
 function requirementsSettings(files) {
-  if (!files['requirements.txt']) { return; }
+  if (!files["requirements.txt"]) {
+    return;
+  }
 
   for (var i in requirements) {
     var regexp = new RegExp(`^${i}(==)?`);
-    if (regexp.test(files['requirements.txt'])) {
+    if (regexp.test(files["requirements.txt"])) {
       return requirements[i];
     }
   }
 }
 
-const settingsTests = [netlifyTomlSettings, gemfileSettings, requirementsSettings, packageSettings];
-
+const settingsTests = [
+  netlifyTomlSettings,
+  gemfileSettings,
+  requirementsSettings,
+  packageSettings
+];
 
 function jekyllWarnings(files) {
-  if (!files['_config.yml']) { return; }
-  if (!files['Gemfile']) { return messages['jekyll with no gemfile']; }
-  if (!files['Gemfile'].match(/('jekyll'|"jekyll")/)) { return messages['jekyll not in gemfile']; }
+  if (!files["_config.yml"]) {
+    return;
+  }
+  if (
+    files["_config.yml"] &&
+    files["package.json"] &&
+    files["package.json"].match(/hexo/)
+  ) {
+    return;
+  }
+  if (!files["Gemfile"]) {
+    return messages["jekyll with no gemfile"];
+  }
+  if (!files["Gemfile"].match(/('jekyll'|"jekyll")/)) {
+    return messages["jekyll not in gemfile"];
+  }
 }
 
 function packageWarnings(files) {
   var data;
-  if (!files['package.json']) { return; }
+  if (!files["package.json"]) {
+    return;
+  }
 
-  try { data = JSON.parse(files['package.json']); } catch(e) { return messages['error parsing package.json']; }
+  try {
+    data = JSON.parse(files["package.json"]);
+  } catch (e) {
+    return messages["error parsing package.json"];
+  }
 
-  if (files['app.coffee'] && !(
-    (data.dependencies && data.dependencies.roots) ||
-    (data.devDependencies && data.devDependencies.roots)
-  )) {
-    return messages['roots not in package.json'];
+  if (
+    files["app.coffee"] &&
+    !(
+      (data.dependencies && data.dependencies.roots) ||
+      (data.devDependencies && data.devDependencies.roots)
+    )
+  ) {
+    return messages["roots not in package.json"];
   }
 }
 
 const warningTests = [jekyllWarnings, packageWarnings];
 
 const messages = {
-  'jekyll with no gemfile':
-
-`It looks like this a **Jekyll** site, but you don't have a **Gemfile**.
+  "jekyll with no gemfile": `It looks like this a **Jekyll** site, but you don't have a **Gemfile**.
 Add a Gemfile with Jekyll to make netlify install the right version of Jekyll
 before running your build.`,
 
-  'jekyll not in gemfile':
-
-`It looks like this is a **Jekyll** site, but your **Gemfile** doesn't list
+  "jekyll not in gemfile": `It looks like this is a **Jekyll** site, but your **Gemfile** doesn't list
 Jekyll as a dependency. Make sure you add **gem "jekyll"** to your Gemfile
 and then run **bundle install**`,
 
-  'roots not in package.json':
-
-`It looks like this is a **Roots** project, but you don't have roots
+  "roots not in package.json": `It looks like this is a **Roots** project, but you don't have roots
 as a dependency in your **package.json**.
 Run **npm install roots --save** to make sure netlify installs roots
 as a dependency before running your build.`,
 
-  'error parsing package.json':
-
-`You have a malformed package.json in your folder. This will most likely lead to a
+  "error parsing package.json": `You have a malformed package.json in your folder. This will most likely lead to a
 broken build on netlify when attempting to install your npm dependencies.`
-}
+};
